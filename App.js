e36106ffdd70f5ad794bc26f4d049b975276bd72
Fabrício José Barrozo <ueredeveloper@gmail.com>
2023-12-20 18:37:16 -0300

    fetch in nhost hasura - working!!!

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import * as React from 'react';
+import { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { BrowserScreen, HomeScreen } from './screens';
+import { fetchDbCurrencies } from './services/db/fetchDbCurrencies';
+
 
 const Stack = createStackNavigator();
 
+
 function MyStack() {
+
   return (
     <Stack.Navigator>
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
       <Stack.Screen name="BrowserScreen" component={BrowserScreen} />
     </Stack.Navigator>
+
   );
 }
 
 
+
 export default function App() {
+
+/*
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetchDbCurrencies();
+      if (result.error) {
+        // Handle error here
+        console.error('Error fetching data:', result.error);
+      } else {
+        // Handle data here
+        console.log('Fetched data:', result.data);
+      }
+    };
+
+    fetchData();
+  }, []);*/
+
+
+
   return (
     <NavigationContainer>
       <MyStack />
