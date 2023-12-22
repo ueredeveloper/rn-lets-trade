7a4f78e5bf54d7cc8afb358ab1c2d49ec43b243e
Fabrício José Barrozo <ueredeveloper@gmail.com>
2023-12-22 17:26:00 -0300

    explain useeffect method in currenciens.js

--- components/Currencies.js
+++ components/Currencies.js
@@ -81,6 +81,7 @@ const Currencies = ({ navigation }) => {
         try {
           // Moedas a serem buscadas
           const coinsToFetch = filteredCoins;
+          //ex: [{"pair": "BTCUSDT", "price": "43663.64000000"},]
           const fetchedCoins = [];
 
           // Itera sobre as moedas para buscar informações
@@ -89,10 +90,20 @@ const Currencies = ({ navigation }) => {
             // Busca as velas (candles) para cada moeda
             const updatedCoins = await Promise.all(
               chunk.map(async coin => {
-                const fetchedCandles = await fetchCandles(coin.pair, 46, interval);
-                const bollingerBands = calculateBollingerBands(20, fetchedCandles);
-                const candles = calculateRecentCandles(bollingerBands.upper.length, fetchedCandles);
+                //ex: coin => {"pair": "CELOUSDT", "price": "0.69000000"}
+          
 
+                // ex: fetchCandles ('ETHDOWNUSDT', 46, '1d')
+                const fetchedCandles = await fetchCandles(coin.pair, 46, interval);
+                // return fetchedCandles -> [{"baseAssetVolume": ".0", "close": "0.", "quoteVolume": "0", "trades": 0, "volume": ""}, {"baseAssetVolume": ...]
+                const bollingerBands = calculateBollingerBands(20, fetchedCandles);
+                // return -> bollingerBands -> {"lower": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,...}
+
+                // ex calculateRecentCandles( 27 [{"baseAssetVolume": "8881899022.0", "close": "0.0", "closeTime": 0, "high": "0.0",
+                const candles = calculateRecentCandles(bollingerBands.upper.length, fetchedCandles);
+                //ex: candles -> [{"baseAssetVolume": "4022022447.0", "close": "0.0", "closeTime": 0, "high": 
+
+                // return ->  {"pair": "BTTUSDT", "price": "0.00277700"} {"lower": 
                 return { ...coin, bollingerBands, candles };
               })
             );
@@ -100,10 +111,12 @@ const Currencies = ({ navigation }) => {
             fetchedCoins.push(...updatedCoins);
             let sortedCoins = sortByBollingersLowerAndCandlesClose(fetchedCoins)
 
+            console.log('sorted conins', sortedCoins)
+
             // Adiciona as moedas atualizadas ao estado utilizando push
             setFilteredCoins(sortedCoins);
 
-            console.log(fetchedCoins.length)
+           // console.log(fetchedCoins.length)
 
             if (i + 50 < coinsToFetch.length) {
               // Aguarda 3 segundos antes de fazer a próxima requisição
